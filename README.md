e1e3075aa847c3065f9d76e8ea3db8de4944c1ee
van <yuzetao@chafou.com>
2023-12-21 22:38:30 +0800

    feat
    * Add lark_font_change.sh
    
    Signed-off-by: van <yuzetao@chafou.com>

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # chatGPT_program
+
+All code is created by chatGPT.
+
+* `lark_font_change.sh`: change lark asar files' css font-famliy, add JB-Mono-ND-MiS
+
---
+++ lark_font_change.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 指定目标目录
+target_directory="/Applications/Lark.app/Contents/Frameworks/Lark Framework.framework/Resources/webcontent"
+
+echo "1. 查找所有asar文件..."
+# 1. 遍历目录，找出所有asar文件
+IFS=$'\n' asar_files=($(find "$target_directory" -type f -name "*.asar"))
+
+echo "2. 解压这些asar文件..."
+# 2. 解压这些asar文件
+for asar_file in "${asar_files[@]}"; do
+    # 提取文件名（不包含后缀）
+    base_name=$(basename "$asar_file" .asar)
+    # 解压asar文件
+    asar extract "$asar_file" "$target_directory/$base_name-asar"
+    echo "   解压: $asar_file -> \"$target_directory/$base_name-asar\""
+done
+
+echo "3. 修改所有.css文件..."
+# 3. 修改所有.css文件中的`font-family:`为`font-family:JB-Mono-ND-MiS,`
+find "$target_directory" -type f -name "*.css" -exec sed -i '' 's/\(font-family:[[:space:]]*\)/\1JB-Mono-ND-MiS, /g' "{}" +
+
+echo "4. 重命名原始.asar文件为.asar.org..."
+# 4. 重命名原始.asar文件为.asar.org
+for asar_file in "${asar_files[@]}"; do
+    mv "$asar_file" "$asar_file.org"
+    echo "   重命名: $asar_file -> \"$asar_file.org\""
+done
+
+echo "5. 重新打包为.asar文件..."
+# 5. 重新打包为.asar文件
+for asar_file in "${asar_files[@]}"; do
+    # 提取文件名（不包含后缀）
+    base_name=$(basename "$asar_file" .asar)
+    # 重新打包文件夹为.asar文件
+    asar pack "$target_directory/$base_name-asar" "$target_directory/$base_name.asar"
+    echo "   重新打包: \"$target_directory/$base_name-asar\" -> \"$target_directory/$base_name.asar\""
+done
+
+echo "任务完成"
