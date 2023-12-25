2ab9214f5f1e0d58639ca3bdc5e7c3f0289b1558
van <yuzetao@chafou.com>
2023-12-25 22:48:33 +0800

    feat
    * Support traversal files to modify
    
    Signed-off-by: van <yuzetao@chafou.com>

--- lark_recall_change.sh
+++ lark_recall_change.sh
@@ -4,7 +4,6 @@
 base_dir="/Applications/Lark.app/Contents/Frameworks/Lark Framework.framework/Resources/webcontent/"
 asar_file="${base_dir}messenger.asar"
 extract_dir="${base_dir}messenger-asar"
-js_file="${extract_dir}/25873/41ec067c.js"
 
 # 检查解压目录是否存在，如果存在则删除
 if [ -d "$extract_dir" ]; then
@@ -20,11 +19,11 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# 修改JS文件
-echo "Modifying JS file..."
-sed -i '' 's/async receivePushMessagesEntities(e,s){/async receivePushMessagesEntities(e,s){try{Object.keys(e.messages).forEach(itemKey=>{if(e.messages[itemKey].isRecalled){e.messages[itemKey].isRecalled=false;delete e.messages[itemKey];}});}catch(e){console.log(e);}/' "$js_file"
+# 修改所有JS文件
+echo "Modifying JS files..."
+find "$extract_dir" -type f -name "*.js" -exec sed -i '' 's/async receivePushMessagesEntities(e,s){/async receivePushMessagesEntities(e,s){try{Object.keys(e.messages).forEach(itemKey=>{if(e.messages[itemKey].isRecalled){e.messages[itemKey].isRecalled=false;delete e.messages[itemKey];}});}catch(e){console.log(e);}/' {} \;
 if [ $? -ne 0 ]; then
-  echo "Failed to modify JS file"
+  echo "Failed to modify JS files"
   exit 1
 fi
 
